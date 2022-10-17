1149168f159817cf94599da9ebc57fed89d0d8a0
ctd <qpflapffhs76@gmail.com>
2022-10-17 15:09:25 +0900

    Fix: Auth Page useEffect 제거

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 
 const Auth = () => {
   const [isLoginPage, setIsLoginPage] = useState(true);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('access_token')) {
-      navigate('/todo');
-    }
-  }, []);
 
   return isLoginPage ? (
     <SignIn setIsLoginPage={setIsLoginPage} />
